b3d46ade9ebc79134d3b8d4c228fe24c987825f3
AtishubhV <40833821+AtishubhV@users.noreply.github.com>
2021-07-29 16:14:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get("/", (req, resp) => {
     //resp.send("working");
     resp.json([{ "Name": "Atishubh", "Age": "35", "Game": "Cricket" }, { "Name": "Mark", "Age": "32", "Game": "Football"}]);
   });
+app.get("/products", (req, resp) => {
+    //resp.send("working");
+    resp.json([{ "Name": "P1", "Age": "1", "Game": "X" }, { "Name": "P2", "Age": "2", "Game": "Y"}]);
+  });
 app.listen(port, console.log(`Server started at port ${port}`));
