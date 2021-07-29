d76eea87eccab98290644f27f09105abbc000429
AtishubhV <40833821+AtishubhV@users.noreply.github.com>
2021-07-29 16:29:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const port = 8083;
 const app = express();
-app.get("/products", (req, resp) => {
+app.get("/products",  cors(), (req, resp) => {
     //resp.send("working");
     resp.json([{ "Name": "P1", "Age": "1", "Game": "X" }, { "Name": "P2", "Age": "2", "Game": "Y"}]);
   });
-app.get("/", (req, resp) => {
+app.get("/",  cors(), (req, resp) => {
     //resp.send("working");
     resp.json([{ "Name": "Atishubh", "Age": "35", "Game": "Cricket" }, { "Name": "Mark", "Age": "32", "Game": "Football"}]);
   });
