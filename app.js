217f369e8129ef24ba3895d191a1e13b13d6b6f2
AtishubhV <40833821+AtishubhV@users.noreply.github.com>
2021-07-29 16:15:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const port = 8083;
 const app = express();
-app.get("/", (req, resp) => {
-    //resp.send("working");
-    resp.json([{ "Name": "Atishubh", "Age": "35", "Game": "Cricket" }, { "Name": "Mark", "Age": "32", "Game": "Football"}]);
-  });
 app.get("/products", (req, resp) => {
     //resp.send("working");
     resp.json([{ "Name": "P1", "Age": "1", "Game": "X" }, { "Name": "P2", "Age": "2", "Game": "Y"}]);
   });
+app.get("/", (req, resp) => {
+    //resp.send("working");
+    resp.json([{ "Name": "Atishubh", "Age": "35", "Game": "Cricket" }, { "Name": "Mark", "Age": "32", "Game": "Football"}]);
+  });
+
 app.listen(port, console.log(`Server started at port ${port}`));
